c84c4ce892c1531f20677c39d105aec9338b082a
kayamash <yamashita@stu.kobe-u.ac.jp>
2018-11-02 16:20:51 +0900

    bug fix

--- bugfix.sh
+++ bugfix.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 branch="dev_kayamash"
 add="git add ."
-commit="git commit -a --allow-emply-message -m ''"
+commit="git commit -m "
+message="bug fix"
 push="git push origin "
 
-eval $add
-eval $commit
+#eval $add
+#eval $commit"${message}"
+#eval $push$branch
+
+git add .
+git commit -m "${message}"
 eval $push$branch
+
+
+
+
