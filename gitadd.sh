4531a91c2a2ca08842443d65dabca407c9a634e0
kayamash <yamashita@stu.kobe-u.ac.jp>
2018-11-02 19:25:28 +0900

    add list

---
+++ gitadd.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+branch="dev_kayamash"
+file1="efficiencyloop.cpp"
+file2="Efficiency.cpp"
+file3="Efficiency.chh"
+file4="CalcEfficiency.cpp"
+file5="CalcEfficiency.chh"
+file6="plotSP.cpp"
+file7="run.sh"
+file8="bsub.sh"
+file9="gitadd.sh"
+add="git add "
+message="add list"
+push="git push origin "
+
+eval $add$file1
+eval $add$file2
+eval $add$file3
+eval $add$file4
+eval $add$file5
+eval $add$file6
+eval $add$file7
+eval $add$file8
+eval $add$file9
+git commit -m "${message}"
+eval $push$branch
+
+
+
+
