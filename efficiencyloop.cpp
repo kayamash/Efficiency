db7fd5d7a3591640e039ae192080446ec2935027
kayamash <yamashita@stu.kobe-u.ac.jp>
2018-11-05 07:48:32 +0900

    add list

--- efficiencyloop.cpp
+++ efficiencyloop.cpp
@@ -26,11 +26,11 @@
 const string trigger = "mu26ivm";
 //const string trigger = "mu4";
 //const string inputfilename = "~/dataset/efficiencysample2.root";
-//const string inputfilename = "/gpfs/fs6001/kayamash/data18_physics_Main_Ztap_hadd.root";
+const string inputfilename = "/gpfs/fs6001/kayamash/data18_physics_Main_Ztap_hadd.root";
 //const string inputfilename = "/gpfs/fs6001/kayamash/efficiency_output/mc16_13TeVZmumu070.root";
 //const string inputfilename = "/gpfs/fs6001/kayamash/dataset/Zmumu300540_hadd.root";
-const string inputfilelist = "/home/kayamash/list/data16_grid.list";
-const string outputfilename = "/gpfs/fs6001/kayamash/Mywork/efficiencyloopoutput/plot4.root";
+//const string inputfilelist = "/home/kayamash/list/data16_grid.list";
+const string outputfilename = "/gpfs/fs6001/kayamash/Mywork/efficiencyloopoutput/plotadd.root";
 const Int_t efficiency_maxenergy = 101;
 const Double_t efficiency_x_err = 0.25;
 const Int_t nhist = 7;
@@ -41,25 +41,25 @@ void efficiencyloop(){
 	Efficiency eff;
 	
 	TFile *output_file = new TFile(outputfilename.c_str(),"RECREATE");
-        /*
+        
 	TFile *tf1 = TFile::Open(inputfilename.c_str(),"read");
 	TTree *tr1 = dynamic_cast<TTree*>(tf1->Get("t_tap"));
-	*/
-        
+	
+        /*
 	TChain *tr1 = new TChain("t_tap");
 	std::ifstream ifs(inputfilelist.c_str());
 	std::string str;
 	while(getline(ifs,str)){
 		tr1->Add(str.c_str());
 	}
-        
+        */
         if(!tr1)cout<<"read failed!"<<endl; 
 	cout<<"Initialize"<<endl;
 	eff.Init(tr1,trigger,24,20,3.0,2.5,0.08,efficiency_maxenergy,efficiency_x_err,nhist,thpitch);
 	cout<<tr1->GetEntries()<<endl;
 	cout<<"Execute"<<endl;
-	for(Int_t event = 0;event < tr1->GetEntries(); event++){
-	//for(Int_t event = 0;event < 100000; event++){
+	//for(Int_t event = 0;event < tr1->GetEntries(); event++){
+	for(Int_t event = 0;event < 100000; event++){
 		eff.Execute(event);
 	}
 	cout<<"Finalize"<<endl;
