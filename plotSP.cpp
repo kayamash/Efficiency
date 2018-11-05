027e38790bd9e3352c449b4c02f0b9a843d8dd20
kayamash <yamashita@stu.kobe-u.ac.jp>
2018-11-05 13:10:54 +0900

    add list

--- plotSP.cpp
+++ plotSP.cpp
@@ -30,6 +30,10 @@ void plotSP(){
 	TChain *chain = new TChain("t_tap");
 	chain->Add(inputfilename.c_str());
 
+	vector<double>  *SPZ = 0;
+	vector<double>  *SPR = 0;
+
+	vector<string>  *mes_name = 0;
 	vector<double>  *probe_mesSA_superPointR_BI = 0;
 	vector<double>  *probe_mesSA_superPointR_BM = 0;
 	vector<double>  *probe_mesSA_superPointR_BO = 0;
@@ -81,6 +85,7 @@ void plotSP(){
 	vector<double>  *probe_mesSA_superPointChi2_BEE = 0;
 	vector<double>  *probe_mesSA_superPointChi2_BME = 0;
 
+	TBranch *b_mes_name;
 	TBranch *b_probe_mesSA_superPointR_BI;
 	TBranch *b_probe_mesSA_superPointR_BM;
 	TBranch *b_probe_mesSA_superPointR_BO;
@@ -132,12 +137,146 @@ void plotSP(){
 	TBranch *b_probe_mesSA_superPointChi2_BEE;
 	TBranch *b_probe_mesSA_superPointChi2_BME;
 
+	fChain->SetBranchAddress("mes_name", &mes_name, &b_mes_name);
+	fChain->SetBranchAddress("probe_mesSA_superPointR_BI", &probe_mesSA_superPointR_BI, &b_probe_mesSA_superPointR_BI);
+	fChain->SetBranchAddress("probe_mesSA_superPointR_BM", &probe_mesSA_superPointR_BM, &b_probe_mesSA_superPointR_BM);
+	fChain->SetBranchAddress("probe_mesSA_superPointR_BO", &probe_mesSA_superPointR_BO, &b_probe_mesSA_superPointR_BO);
+	fChain->SetBranchAddress("probe_mesSA_superPointR_EI", &probe_mesSA_superPointR_EI, &b_probe_mesSA_superPointR_EI);
+	fChain->SetBranchAddress("probe_mesSA_superPointR_EM", &probe_mesSA_superPointR_EM, &b_probe_mesSA_superPointR_EM);
+	fChain->SetBranchAddress("probe_mesSA_superPointR_EO", &probe_mesSA_superPointR_EO, &b_probe_mesSA_superPointR_EO);
+	fChain->SetBranchAddress("probe_mesSA_superPointR_EE", &probe_mesSA_superPointR_EE, &b_probe_mesSA_superPointR_EE);
+	fChain->SetBranchAddress("probe_mesSA_superPointR_CSC", &probe_mesSA_superPointR_CSC, &b_probe_mesSA_superPointR_CSC);
+	fChain->SetBranchAddress("probe_mesSA_superPointR_BEE", &probe_mesSA_superPointR_BEE, &b_probe_mesSA_superPointR_BEE);
+	fChain->SetBranchAddress("probe_mesSA_superPointR_BME", &probe_mesSA_superPointR_BME, &b_probe_mesSA_superPointR_BME);
+	fChain->SetBranchAddress("probe_mesSA_superPointZ_BI", &probe_mesSA_superPointZ_BI, &b_probe_mesSA_superPointZ_BI);
+	fChain->SetBranchAddress("probe_mesSA_superPointZ_BM", &probe_mesSA_superPointZ_BM, &b_probe_mesSA_superPointZ_BM);
+	fChain->SetBranchAddress("probe_mesSA_superPointZ_BO", &probe_mesSA_superPointZ_BO, &b_probe_mesSA_superPointZ_BO);
+	fChain->SetBranchAddress("probe_mesSA_superPointZ_EI", &probe_mesSA_superPointZ_EI, &b_probe_mesSA_superPointZ_EI);
+	fChain->SetBranchAddress("probe_mesSA_superPointZ_EM", &probe_mesSA_superPointZ_EM, &b_probe_mesSA_superPointZ_EM);
+	fChain->SetBranchAddress("probe_mesSA_superPointZ_EO", &probe_mesSA_superPointZ_EO, &b_probe_mesSA_superPointZ_EO);
+	fChain->SetBranchAddress("probe_mesSA_superPointZ_EE", &probe_mesSA_superPointZ_EE, &b_probe_mesSA_superPointZ_EE);
+	fChain->SetBranchAddress("probe_mesSA_superPointZ_CSC", &probe_mesSA_superPointZ_CSC, &b_probe_mesSA_superPointZ_CSC);
+	fChain->SetBranchAddress("probe_mesSA_superPointZ_BEE", &probe_mesSA_superPointZ_BEE, &b_probe_mesSA_superPointZ_BEE);
+	fChain->SetBranchAddress("probe_mesSA_superPointZ_BME", &probe_mesSA_superPointZ_BME, &b_probe_mesSA_superPointZ_BME);
+	fChain->SetBranchAddress("probe_mesSA_superPointSlope_BI", &probe_mesSA_superPointSlope_BI, &b_probe_mesSA_superPointSlope_BI);
+	fChain->SetBranchAddress("probe_mesSA_superPointSlope_BM", &probe_mesSA_superPointSlope_BM, &b_probe_mesSA_superPointSlope_BM);
+	fChain->SetBranchAddress("probe_mesSA_superPointSlope_BO", &probe_mesSA_superPointSlope_BO, &b_probe_mesSA_superPointSlope_BO);
+	fChain->SetBranchAddress("probe_mesSA_superPointSlope_EI", &probe_mesSA_superPointSlope_EI, &b_probe_mesSA_superPointSlope_EI);
+	fChain->SetBranchAddress("probe_mesSA_superPointSlope_EM", &probe_mesSA_superPointSlope_EM, &b_probe_mesSA_superPointSlope_EM);
+	fChain->SetBranchAddress("probe_mesSA_superPointSlope_EO", &probe_mesSA_superPointSlope_EO, &b_probe_mesSA_superPointSlope_EO);
+	fChain->SetBranchAddress("probe_mesSA_superPointSlope_EE", &probe_mesSA_superPointSlope_EE, &b_probe_mesSA_superPointSlope_EE);
+	fChain->SetBranchAddress("probe_mesSA_superPointSlope_CSC", &probe_mesSA_superPointSlope_CSC, &b_probe_mesSA_superPointSlope_CSC);
+	fChain->SetBranchAddress("probe_mesSA_superPointSlope_BEE", &probe_mesSA_superPointSlope_BEE, &b_probe_mesSA_superPointSlope_BEE);
+	fChain->SetBranchAddress("probe_mesSA_superPointSlope_BME", &probe_mesSA_superPointSlope_BME, &b_probe_mesSA_superPointSlope_BME);
+	fChain->SetBranchAddress("probe_mesSA_superPointIntercept_BI", &probe_mesSA_superPointIntercept_BI, &b_probe_mesSA_superPointIntercept_BI);
+	fChain->SetBranchAddress("probe_mesSA_superPointIntercept_BM", &probe_mesSA_superPointIntercept_BM, &b_probe_mesSA_superPointIntercept_BM);
+	fChain->SetBranchAddress("probe_mesSA_superPointIntercept_BO", &probe_mesSA_superPointIntercept_BO, &b_probe_mesSA_superPointIntercept_BO);
+	fChain->SetBranchAddress("probe_mesSA_superPointIntercept_EI", &probe_mesSA_superPointIntercept_EI, &b_probe_mesSA_superPointIntercept_EI);
+	fChain->SetBranchAddress("probe_mesSA_superPointIntercept_EM", &probe_mesSA_superPointIntercept_EM, &b_probe_mesSA_superPointIntercept_EM);
+	fChain->SetBranchAddress("probe_mesSA_superPointIntercept_EO", &probe_mesSA_superPointIntercept_EO, &b_probe_mesSA_superPointIntercept_EO);
+	fChain->SetBranchAddress("probe_mesSA_superPointIntercept_EE", &probe_mesSA_superPointIntercept_EE, &b_probe_mesSA_superPointIntercept_EE);
+	fChain->SetBranchAddress("probe_mesSA_superPointIntercept_CSC", &probe_mesSA_superPointIntercept_CSC, &b_probe_mesSA_superPointIntercept_CSC);
+	fChain->SetBranchAddress("probe_mesSA_superPointIntercept_BEE", &probe_mesSA_superPointIntercept_BEE, &b_probe_mesSA_superPointIntercept_BEE);
+	fChain->SetBranchAddress("probe_mesSA_superPointIntercept_BME", &probe_mesSA_superPointIntercept_BME, &b_probe_mesSA_superPointIntercept_BME);
+	fChain->SetBranchAddress("probe_mesSA_superPointChi2_BI", &probe_mesSA_superPointChi2_BI, &b_probe_mesSA_superPointChi2_BI);
+	fChain->SetBranchAddress("probe_mesSA_superPointChi2_BM", &probe_mesSA_superPointChi2_BM, &b_probe_mesSA_superPointChi2_BM);
+	fChain->SetBranchAddress("probe_mesSA_superPointChi2_BO", &probe_mesSA_superPointChi2_BO, &b_probe_mesSA_superPointChi2_BO);
+	fChain->SetBranchAddress("probe_mesSA_superPointChi2_EI", &probe_mesSA_superPointChi2_EI, &b_probe_mesSA_superPointChi2_EI);
+	fChain->SetBranchAddress("probe_mesSA_superPointChi2_EM", &probe_mesSA_superPointChi2_EM, &b_probe_mesSA_superPointChi2_EM);
+	fChain->SetBranchAddress("probe_mesSA_superPointChi2_EO", &probe_mesSA_superPointChi2_EO, &b_probe_mesSA_superPointChi2_EO);
+	fChain->SetBranchAddress("probe_mesSA_superPointChi2_EE", &probe_mesSA_superPointChi2_EE, &b_probe_mesSA_superPointChi2_EE);
+	fChain->SetBranchAddress("probe_mesSA_superPointChi2_CSC", &probe_mesSA_superPointChi2_CSC, &b_probe_mesSA_superPointChi2_CSC);
+	fChain->SetBranchAddress("probe_mesSA_superPointChi2_BEE", &probe_mesSA_superPointChi2_BEE, &b_probe_mesSA_superPointChi2_BEE);
+	fChain->SetBranchAddress("probe_mesSA_superPointChi2_BME", &probe_mesSA_superPointChi2_BME, &b_probe_mesSA_superPointChi2_BME);
+
 	for(Int_t event = 0;event < chain->GetEntries();event++){
 		chain->GetEntry(event);
+		Double_t buf_probe_mesSA_superPointR_BI = -99999;
+		Double_t buf_probe_mesSA_superPointR_BM = -99999;
+		Double_t buf_probe_mesSA_superPointR_BO = -99999;
+		Double_t buf_probe_mesSA_superPointR_EI = -99999;
+		Double_t buf_probe_mesSA_superPointR_EM = -99999;
+		Double_t buf_probe_mesSA_superPointR_EO = -99999;
+		Double_t buf_probe_mesSA_superPointR_EE = -99999;
+		Double_t buf_probe_mesSA_superPointR_CSC = -99999;
+		Double_t buf_probe_mesSA_superPointR_BEE = -99999;
+		Double_t buf_probe_mesSA_superPointR_BME = -99999;
+		Double_t buf_probe_mesSA_superPointZ_BI = -99999;
+		Double_t buf_probe_mesSA_superPointZ_BM = -99999;
+		Double_t buf_probe_mesSA_superPointZ_BO = -99999;
+		Double_t buf_probe_mesSA_superPointZ_EI = -99999;
+		Double_t buf_probe_mesSA_superPointZ_EM = -99999;
+		Double_t buf_probe_mesSA_superPointZ_EO = -99999;
+		Double_t buf_probe_mesSA_superPointZ_EE = -99999;
+		Double_t buf_probe_mesSA_superPointZ_CSC = -99999;
+		Double_t buf_probe_mesSA_superPointZ_BEE = -99999;
+		Double_t buf_probe_mesSA_superPointZ_BME = -99999;
 		for(Int_t mes = 0;mes < probe_mesSAsuperPointR_BI;mes++){
-			
+			if(mes_name->at(mes) == mesSA){
+				buf_probe_mesSA_superPointR_BI = probe_mesSA_superPointR_BI->at(mes);
+				buf_probe_mesSA_superPointR_BM = probe_mesSA_superPointR_BM->at(mes);
+				buf_probe_mesSA_superPointR_BO = probe_mesSA_superPointR_BO->at(mes);
+				buf_probe_mesSA_superPointR_EI = probe_mesSA_superPointR_EI->at(mes);
+				buf_probe_mesSA_superPointR_EM = probe_mesSA_superPointR_EM->at(mes);
+				buf_probe_mesSA_superPointR_EO = probe_mesSA_superPointR_EO->at(mes);
+				buf_probe_mesSA_superPointR_EE = probe_mesSA_superPointR_EE->at(mes);
+				buf_probe_mesSA_superPointR_CSC = probe_mesSA_superPointR_CSC->at(mes);
+				buf_probe_mesSA_superPointR_BEE = probe_mesSA_superPointR_BEE->at(mes);
+				buf_probe_mesSA_superPointR_BME = probe_mesSA_superPointR_BME->at(mes);
+				buf_probe_mesSA_superPointZ_BI = probe_mesSA_superPointZ_BI->at(mes);
+				buf_probe_mesSA_superPointZ_BM = probe_mesSA_superPointZ_BM->at(mes);
+				buf_probe_mesSA_superPointZ_BO = probe_mesSA_superPointZ_BO->at(mes);
+				buf_probe_mesSA_superPointZ_EI = probe_mesSA_superPointZ_EI->at(mes);
+				buf_probe_mesSA_superPointZ_EM = probe_mesSA_superPointZ_EM->at(mes);
+				buf_probe_mesSA_superPointZ_EO = probe_mesSA_superPointZ_EO->at(mes);
+				buf_probe_mesSA_superPointZ_EE = probe_mesSA_superPointZ_EE->at(mes);
+				buf_probe_mesSA_superPointZ_CSC = probe_mesSA_superPointZ_CSC->at(mes);
+				buf_probe_mesSA_superPointZ_BEE = probe_mesSA_superPointZ_BEE->at(mes);
+				buf_probe_mesSA_superPointZ_BME = probe_mesSA_superPointZ_BME->at(mes);
+			}
+		}
+		if(buf_probe_mesSA_superPointR_BI != -99999 && buf_probe_mesSA_superPointZ_BI != -99999){
+			SPR.push_back(buf_probe_mesSA_superPointR_BI);
+			SPZ.push_back(buf_probe_mesSA_superPointZ_BI);
+		}
+		if(buf_probe_mesSA_superPointR_BM != -99999 && buf_probe_mesSA_superPointZ_BM != -99999){
+			SPR.push_back(buf_probe_mesSA_superPointR_BM);
+			SPZ.push_back(buf_probe_mesSA_superPointZ_BM);
+		}
+		if(buf_probe_mesSA_superPointR_BO != -99999 && buf_probe_mesSA_superPointZ_BO != -99999){
+			SPR.push_back(buf_probe_mesSA_superPointR_BO);
+			SPZ.push_back(buf_probe_mesSA_superPointZ_BO);
+		}
+		if(buf_probe_mesSA_superPointR_EI != -99999 && buf_probe_mesSA_superPointZ_EI != -99999){
+			SPR.push_back(buf_probe_mesSA_superPointR_EI);
+			SPZ.push_back(buf_probe_mesSA_superPointZ_EI);
+		}
+		if(buf_probe_mesSA_superPointR_EM != -99999 && buf_probe_mesSA_superPointZ_EM != -99999){
+			SPR.push_back(buf_probe_mesSA_superPointR_EM);
+			SPZ.push_back(buf_probe_mesSA_superPointZ_EM);
+		}
+		if(buf_probe_mesSA_superPointR_EO != -99999 && buf_probe_mesSA_superPointZ_EO != -99999){
+			SPR.push_back(buf_probe_mesSA_superPointR_EO);
+			SPZ.push_back(buf_probe_mesSA_superPointZ_EO);
+		}
+		if(buf_probe_mesSA_superPointR_EE != -99999 && buf_probe_mesSA_superPointZ_EE != -99999){
+			SPR.push_back(buf_probe_mesSA_superPointR_EE);
+			SPZ.push_back(buf_probe_mesSA_superPointZ_EE);
+		}
+		if(buf_probe_mesSA_superPointR_CSC != -99999 && buf_probe_mesSA_superPointZ_CSC != -99999){
+			SPR.push_back(buf_probe_mesSA_superPointR_CSC);
+			SPZ.push_back(buf_probe_mesSA_superPointZ_CSC);
+		}
+		if(buf_probe_mesSA_superPointR_BEE != -99999 && buf_probe_mesSA_superPointZ_BEE != -99999){
+			SPR.push_back(buf_probe_mesSA_superPointR_BEE);
+			SPZ.push_back(buf_probe_mesSA_superPointZ_BEE);
+		}
+		if(buf_probe_mesSA_superPointR_BME != -99999 && buf_probe_mesSA_superPointZ_BME != -99999){
+			SPR.push_back(buf_probe_mesSA_superPointR_BME);
+			SPZ.push_back(buf_probe_mesSA_superPointZ_BME);
 		}
 	}
 
-
+	TGraph *gr = new TGraph(SPR.size(),&(SPR.at(0)),&(SPZ.at(0)));
+	gr->Draw();
 }
