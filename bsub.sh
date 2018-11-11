2928d7fd0f8d81e4fc296cb000a5f0ea5d591c89
kayamash <yamashita@stu.kobe-u.ac.jp>
2018-11-11 21:21:47 +0900

    add list

--- bsub.sh
+++ bsub.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 run="root -l -b -q efficiencyloop.cpp++"
-bsub="bsub -q 4h -o ~/log/outnewClac.log -e ~/log/errnewCalc.log "
+bsub="bsub -q 4h -o ~/log/outnewClacnoselection.log -e ~/log/errnewCalnocselection.log "
 clean="rm efficiencyloop_*"
 eval $bsub$run
 eval $clean
