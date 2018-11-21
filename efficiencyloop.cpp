5cadd8babb76ce7ccb094c298e7634a53ad5fabe
kayamash <yamashita@stu.kobe-u.ac.jp>
2018-11-22 07:22:51 +0900

    add list

--- efficiencyloop.cpp
+++ efficiencyloop.cpp
@@ -31,12 +31,12 @@ Int_t proc = 3;
 
 //const string inputfilename = "~/dataset/efficiencysample2.root";
 //const string inputfilelist = "/home/kayamash/efflist/Zmumu300540_hadd.list";
-//const string inputfilelist = "/home/kayamash/efflist/data18_physics_Main_Ztap.list";
+const string inputfilelist = "/home/kayamash/efflist/data18_physics_Main_Ztap.list";
 //const string inputfilelist = "/home/kayamash/efflist/Calcdata18.list";
-const string inputfilelist = "/home/kayamash/efflist/newmc16345099.list"; 
+//const string inputfilelist = "/home/kayamash/efflist/newmc16345099.list"; 
 //const string outputfilename = "/gpfs/fs6001/kayamash/Mywork/efficiencyloopoutput/Calcdata18mu26ivm0.root";
-//const string outputfilename = "/gpfs/fs6001/kayamash/Mywork/efficiencyloopoutput/newdata18_physics_Main_Ztap.root";
-const string outputfilename = "/gpfs/fs6001/kayamash/Mywork/efficiencyloopoutput/newmc16345099.root";
+const string outputfilename = "/gpfs/fs6001/kayamash/Mywork/efficiencyloopoutput/newdata18_physics_Main_Ztap.root";
+//const string outputfilename = "/gpfs/fs6001/kayamash/Mywork/efficiencyloopoutput/newmc16345099.root";
 const Int_t efficiency_maxenergy = 101;
 const Double_t efficiency_x_err = 0.25;
 const Int_t nhist = 1;
