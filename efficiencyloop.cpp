2b26ecba1521396fa9c97f46d8ce14193f269d1e
kayamash <yamashita@stu.kobe-u.ac.jp>
2018-11-27 14:13:09 +0900

    bug fix

--- efficiencyloop.cpp
+++ efficiencyloop.cpp
@@ -29,17 +29,16 @@ const string trigger = "mu26ivm";
 //Jpsitap == 1,Ztap == 3
 Int_t proc = 3;
 
-//const string inputfilename = "~/dataset/efficiencysample2.root";
-//const string inputfilelist = "/home/kayamash/efflist/Zmumu300540_hadd.list";
+//const string inputfilelist = "/home/kayamash/efflist/mc426336.list";
 const string inputfilelist = "/home/kayamash/efflist/data18_physics_Main_Ztap.list";
-//const string inputfilelist = "/home/kayamash/efflist/Calcdata18.list";
+//const string inputfilelist = "/home/kayamash/efflist/newJpsi_noMdtCsm1k.list";
 //const string inputfilelist = "/home/kayamash/efflist/newmc16345099.list"; 
-//const string outputfilename = "/gpfs/fs6001/kayamash/Mywork/efficiencyloopoutput/Calcdata18mu26ivm0.root";
+//const string outputfilename = "/gpfs/fs6001/kayamash/Mywork/efficiencyloopoutput/newJpsi_noMdtCsm1k.root";
 const string outputfilename = "/gpfs/fs6001/kayamash/Mywork/efficiencyloopoutput/newdata18_physics_Main_Ztap.root";
-//const string outputfilename = "/gpfs/fs6001/kayamash/Mywork/efficiencyloopoutput/newmc16345099.root";
+//const string outputfilename = "/gpfs/fs6001/kayamash/Mywork/efficiencyloopoutput/mc426336.root";
 const Int_t efficiency_maxenergy = 101;
 const Double_t efficiency_x_err = 0.25;
-const Int_t nhist = 1;
+const Int_t nhist = 8;
 const Int_t thpitch = 4;
 
 //main function
