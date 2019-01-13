13a97e1de38f75603d3020932ee3803abbe7c63a
kayamash <yamashita@stu.kobe-u.ac.jp>
2019-01-14 01:12:14 +0900

    add new hist

--- Efficiency.cpp
+++ Efficiency.cpp
@@ -405,7 +405,7 @@ void Efficiency::Execute(Int_t ev){
           if(pSA_sAddress == 1){
                for(Int_t mdthit = 0; mdthit < (signed int)pSA_mdtZ->size(); mdthit++){
                     m_h_mdtphi_LS.at(i)->Fill(pSA_mdtPhi->at(mdthit));
-                    cout<<pSA_mdtR->at(mdthit)<<"   "<<pSA_mdtPhi->at(mdthit);
+                    cout<<pSA_mdtR->at(mdthit)<<endl;
                     if(4000 < pSA_mdtR->at(mdthit) && pSA_mdtR->at(mdthit) < 5000 && pSA_mdtPhi->at(mdthit) < -0.6 && pSA_mdtPhi->at(mdthit) > -0.8){
                          nummdtspecial++;
                     }
