8d9bd3acf859e7e2d8e6e24226370ebc5cd4dd03
kayamash <yamashita@stu.kobe-u.ac.jp>
2019-03-04 16:11:19 +0900

    bug fix

--- gitadd.sh
+++ gitadd.sh
@@ -9,19 +9,20 @@ file7="run.sh"
 file8="bsub.sh"
 file9="gitadd.sh"
 file10="scp.sh"
-add="git add "
+add="git add -A"
 message="bug fix"
 push="git push origin "
 
-eval $add$file1
-eval $add$file2
-eval $add$file3
-eval $add$file4
-eval $add$file5
-eval $add$file7
-eval $add$file8
-eval $add$file9
-eval $add$file10
+#eval $add$file1
+#eval $add$file2
+#eval $add$file3
+#eval $add$file4
+#eval $add$file5
+#eval $add$file7
+#eval $add$file8
+#eval $add$file9
+#eval $add$file10
+git add -A
 git commit -m "${message}"
 eval $push$branch
 
