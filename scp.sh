2da22f236a5d4f30f8a52205c1723fb39ab7c242
kayamash <yamashita@stu.kobe-u.ac.jp>
2019-03-29 18:59:42 +0900

    bug fix

--- scp.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-#scp CalcEff.* Efficiency.* efficiencyloop.* kayamash@lxatut01.cern.ch:/home/kayamash/code/Efficiency/
-scp -r -o "ProxyCommand ssh kayamash@lxplus.cern.ch -W %h:%p" CalcEff.* Efficiency.* efficiencyloop.* kayamash@lxatut01.cern.ch:/home/kayamash/code/Efficiency/ 
