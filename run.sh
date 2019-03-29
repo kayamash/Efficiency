04eecc695087dd208a7b0c0ac913f44c490a9866
kayamash <yamashita@stu.kobe-u.ac.jp>
2019-03-29 16:46:45 +0900

    bug fix

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-run="root -l -b -q efficiencyloop.cpp++"
+run="nohup root -l -b -q efficiencyloop.cpp++ > /home/kayamash/log/logdata18test.txt &"
 clean="rm -r efficiencyloop_*"
 eval $run
 eval $clean
