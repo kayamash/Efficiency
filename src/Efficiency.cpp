9adf56e59f09e806570ba82fc1b676a834b53d43
kayamash <yamashita@stu.kobe-u.ac.jp>
2019-05-10 23:08:55 +0900

    bug fix

--- src/Efficiency.cpp
+++ src/Efficiency.cpp
@@ -32,8 +32,10 @@ const Double_t pt_threshold[4] = {3.38,1.25,3.17,3.41};//MU4
 //const Double_t pt_threshold[4] = {5.17,3.25,4.69,5.14};//MU6
 //const Double_t pt_threshold[4] = {15.87,10.73,12.21,15.87};//MU20
 const Double_t LargeRegion[9] = {0.25,0.55,1.025,1.325,1.80,2.125,2.575,2.90,TMath::Pi()};
-const Double_t correctionAlpha[3] = {0,0.0283101,-0.00993997};//1SP 2SP 3SP
-const Double_t correctionBeta[3] = {0,0.024428,-0.00257345};//1SP 2SP 3SP
+//const Double_t correctionAlpha[3] = {0,0.0283101,-0.00993997};//1SP 2SP 3SP
+//const Double_t correctionBeta[3] = {0,0.024428,-0.00257345};//1SP 2SP 3SP
+const Double_t correctionAlpha[3] = {0.,0.,0.};//1SP 2SP 3SP
+const Double_t correctionBeta[3] = {0.,0.,0.};//1SP 2SP 3SP
 
 void Efficiency::Init(std::string name,const Int_t np,const Int_t ne,const Double_t mp,const Double_t me,Double_t req,Int_t max,Double_t err,Int_t proc){
      m_nbin_phi = np;
